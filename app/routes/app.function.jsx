dd75cd675b70a4587348064646c4b0ef34a58c5e
rahulmeghaniprime-afk <rahulmeghani.prime@gmail.com>
2026-06-05 14:56:02 +0530

    createforit

--- app/routes/app.function.jsx
+++ app/routes/app.function.jsx
@@ -31,7 +31,7 @@ export const loader = async ({ request }) => {
 
   const existingCustomization =
     data?.data?.paymentCustomizations?.nodes?.find(
-      (item) => item.shopifyFunction?.app?.handle === 'custompayment2'
+      (item) => item.shopifyFunction?.app?.handle === 'custompayment'
     );
 
   let created = false;
